af826aa87c80286a2da7af5ca23eacb328af2567
Gustaf Rosenblad <gustaf@ccode.se>
2025-08-16 15:59:24 +0200

    feat: Optimized gRPC client configuration for server side

--- Sources/GoogleCloudPubSub/PubSubService.swift
+++ Sources/GoogleCloudPubSub/PubSubService.swift
@@ -38,7 +38,32 @@ actor PubSubService {
       self.grpcClient = GRPCClient(
         transport: try .http2NIOPosix(
           target: .dns(host: components[0], port: port),
-          transportSecurity: .plaintext
+          transportSecurity: .plaintext,
+          config: .defaults { config in
+            config.backoff = .init(
+              initial: .milliseconds(100),
+              max: .seconds(1),
+              multiplier: 1.6,
+              jitter: 0.2
+            )
+            config.connection = .init(
+              maxIdleTime: .seconds(30 * 60),
+              keepalive: .init(
+                time: .seconds(30),
+                timeout: .seconds(5),
+                allowWithoutCalls: true
+              )
+            )
+          },
+          serviceConfig: .init(
+            methodConfig: [
+              .init(
+                names: [.init(service: "")],  // Empty service means all methods
+                waitForReady: true,
+                timeout: .seconds(60)
+              )
+            ]
+          )
         )
       )
       self.isUsingEmulator = true
@@ -53,7 +78,32 @@ actor PubSubService {
       self.grpcClient = GRPCClient(
         transport: try .http2NIOPosix(
           target: .dns(host: "pubsub.googleapis.com"),
-          transportSecurity: .tls
+          transportSecurity: .tls,
+          config: .defaults { config in
+            config.backoff = .init(
+              initial: .milliseconds(100),
+              max: .seconds(1),
+              multiplier: 1.6,
+              jitter: 0.2
+            )
+            config.connection = .init(
+              maxIdleTime: .seconds(30 * 60),
+              keepalive: .init(
+                time: .seconds(30),
+                timeout: .seconds(5),
+                allowWithoutCalls: true
+              )
+            )
+          },
+          serviceConfig: .init(
+            methodConfig: [
+              .init(
+                names: [.init(service: "")],  // Empty service means all methods
+                waitForReady: true,
+                timeout: .seconds(60)
+              )
+            ]
+          )
         ),
         interceptors: [
           AuthorizationClientInterceptor(authorization: authorization)
